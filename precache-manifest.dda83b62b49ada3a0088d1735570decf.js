8e55d3d43406bb9cd81ad041ccceddef1bc32983
Yuvraj Kaushal <ykaushal364@gmail.com>
2021-06-03 23:27:22 +0530

    Updates

---
+++ precache-manifest.dda83b62b49ada3a0088d1735570decf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f258f218d0937cdec636a57665e9d923",
+    "url": "/1st_ReactJS/index.html"
+  },
+  {
+    "revision": "493fa4f4634304726b0e",
+    "url": "/1st_ReactJS/static/css/main.b6c5b9f5.chunk.css"
+  },
+  {
+    "revision": "1aad54b3b9d3801b31c2",
+    "url": "/1st_ReactJS/static/js/2.709bd807.chunk.js"
+  },
+  {
+    "revision": "493fa4f4634304726b0e",
+    "url": "/1st_ReactJS/static/js/main.ee514a57.chunk.js"
+  },
+  {
+    "revision": "1730df28486d2261b7f0",
+    "url": "/1st_ReactJS/static/js/runtime-main.c97e28eb.js"
+  }
+]);
